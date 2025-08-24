cb68d0770f56e3abf828608168fe0751c69e3a2f
Mualif Syarifuddin <syarifuddinmualif@gmail.com>
2025-08-24 15:24:49 +0700

    _view belum benar

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,8 +13,8 @@ class UserSeeder extends Seeder
     {
         // Admin
         User::create([
-            'name' => 'Admin Utama',
-            'email' => 'admin@example.com',
+            'name' => 'Admin Utama1',
+            'email' => 'admin1@example.com',
             'password' => Hash::make('123456789'), // ubah password sesuai kebutuhan
             'role' => 'admin',
             'nomor_whatsapp' => '081234567890',
@@ -22,8 +22,8 @@ class UserSeeder extends Seeder
 
         // Kepala Bagian
         User::create([
-            'name' => 'Kepala Bagian Produksi',
-            'email' => 'kepala@example.com',
+            'name' => 'Kepala Bagian Produksi1',
+            'email' => 'kepala1@example.com',
             'password' => Hash::make('123456789'),
             'role' => 'kepala_bagian',
             'nomor_whatsapp' => '081234567891',
@@ -31,8 +31,8 @@ class UserSeeder extends Seeder
 
         // Mekanik
         User::create([
-            'name' => 'Mekanik 1',
-            'email' => 'mekanik@example.com',
+            'name' => 'Mekanik 2',
+            'email' => 'mekanik2@example.com',
             'password' => Hash::make('123456789'),
             'role' => 'mekanik',
             'nomor_whatsapp' => '081234567892',
